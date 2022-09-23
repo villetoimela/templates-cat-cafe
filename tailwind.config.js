4b0be41a96ed71bb8742a648a62d82f6a8462cef
villetoimela <ville.toimela@hiisi.digital>
2022-09-23 12:00:10 +0300

    added google fonts (montserrat)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "ui-sans-serif", "system-ui"],
+        serif: ["ui-serif", "Georgia"],
+        mono: ["ui-monospace", "SFMono-Regular"],
+      },
+    },
   },
   plugins: [],
 }
