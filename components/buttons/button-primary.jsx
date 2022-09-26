7f0fe35394638aef378079f19e50b9e3554261ad
villetoimela <ville.toimela@hiisi.digital>
2022-09-26 13:26:55 +0300

    added styles as props in button-primary to make it reusable

--- components/buttons/button-primary.jsx
+++ components/buttons/button-primary.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonPrimary = (props) => {
     return (
-        <a href="#" className="px-8 py-3 text-center font-bold text-sm sm:text-base text-white bg-pink-500 border-2 border-pink-500 rounded-xl hover:bg-pink-600 duration-200">{props.text}</a>
+        <a href={props.url} className={` ${props.styles} px-8 py-3 text-center text-sm sm:text-base duration-200 rounded-xl `}>{props.text}</a>
     )
 }
 
