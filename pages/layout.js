4326fa668aff6701d3e182dadd44122f22c1b6e8
villetoimela <ville.toimela@hiisi.digital>
2022-10-03 09:31:04 +0300

    added some padding top to layout

--- pages/layout.js
+++ pages/layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Navbar />
-            <main> {children} </main>
+            <main className="pt-[70px]"> {children} </main>
             <Footer />
         </>
     )
