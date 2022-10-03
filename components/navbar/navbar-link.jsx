5f97c8e7ea05d02537b57bffbf014faed5b2a8db
villetoimela <ville.toimela@hiisi.digital>
2022-10-03 09:32:02 +0300

    added navbarLink href as props

--- components/navbar/navbar-link.jsx
+++ components/navbar/navbar-link.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const NavbarLink = (props) => {
     return (
         <li className="nav-item">
-            <Link href={"/"}>
+            <Link href={`/${props.to}`}>
                 <a className="text-2xl font-AmaticSC tracking-wide cursor-pointer font-bold text-white transition-colors duration-200 hover:text-purple-400">
                     <span className="ml-2">{props.text}</span>
                 </a>
