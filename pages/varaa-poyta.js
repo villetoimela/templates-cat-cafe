117d37046c5099bcbc0ff12087aa596f5ba5e1a4
villetoimela <ville.toimela@hiisi.digital>
2022-10-04 09:50:12 +0300

    added new page for booking a table

---
+++ pages/varaa-poyta.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Booking from '../components/booking'
+import PrimaryHeader from '../components/cat-showcase/cat-showcase-header'
+
+const VaraaPoyta = () => {
+    return (
+        <section className="py-44 bg-gradient-to-t from-purple-200 via-purple-50 to-purple-200">
+            <div className="flex flex-col px-8 mx-auto max-w-7xl xl:px-12">
+                <PrimaryHeader
+                    title="Varaa pöytä halutessasi täältä!"
+                    text=""
+                />
+
+                <Booking
+                    mainText="Varmista paikkasi oheisesta varausjärjestelmästä"
+                    text="Kissakavereidemme seuraan pääsee yleisestiottaen herkuttelemaan kävelemällä kahvilaan sisään, mutta varsinkin viikonloppuisin tai loma-aikana pöydän varamaaisella olet taatusti varannut paikkasi meiltä. Voit myös soittaa suoraan meille ja kysellä nykyisistä asiakasmääristä!"
+                />
+            </div>
+        </section>
+    )
+}
+
+export default VaraaPoyta
