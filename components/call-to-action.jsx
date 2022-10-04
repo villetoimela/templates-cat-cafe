d81a679dbf11f59713d7b6873cfef6770aa4040f
villetoimela <ville.toimela@hiisi.digital>
2022-10-04 15:51:01 +0300

    deleted that react-reveal from CallToAction h2

--- components/call-to-action.jsx
+++ components/call-to-action.jsx
@@ -9,21 +9,20 @@ const CallToAction = (props) => {
             <div className="h-36 w-36 rounded-full bg-purple-400 absolute blur-3xl bottom-0"></div>
             <div className="h-24 w-24 rounded-full bg-purple-400 absolute blur-3xl top-0 right-0"></div>
             <div className="max-w-xs px-5 sm:max-w-5xl mx-auto flex lg:flex-row flex-col items-start sm:items-center lg:items-end relative">
-                <Fade bottom cascade duration={600}>
-                    <h2 className="text-white sm:pl-10 sm:pr-16 font-thin text-4xl sm:text-6xl lg:text-left sm:text-center relative leading-none">{props.mainText}</h2>
+
+                <h2 className="text-white sm:pl-10 sm:pr-16 font-thin text-4xl sm:text-6xl lg:text-left sm:text-center relative leading-none">{props.mainText}</h2>
 
 
-                    <div className="lg:w-1/2 w-full flex flex-col justify-center relative items-end">
-                        <div className="lg:w-auto w-full flex flex-col sm:items-center sm:text-center lg:text-left">
-                            <ButtonSecondary
-                                styles="lg:w-full mt-10 mb-3 text-base font-normal"
-                                text={props.buttonText}
-                                url={props.url}
-                            />
-                            <p className="text-gray-300 mt-2 sm:text-center"> {props.text} ❤️</p>
-                        </div>
+                <div className="lg:w-1/2 w-full flex flex-col justify-center relative items-end">
+                    <div className="lg:w-auto w-full flex flex-col sm:items-center sm:text-center lg:text-left">
+                        <ButtonSecondary
+                            styles="lg:w-full mt-10 mb-3 text-base font-normal"
+                            text={props.buttonText}
+                            url={props.url}
+                        />
+                        <p className="text-gray-300 mt-2 sm:text-center"> {props.text} ❤️</p>
                     </div>
-                </Fade>
+                </div>
             </div>
         </section>
     )
