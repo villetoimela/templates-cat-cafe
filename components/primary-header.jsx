98786b934e355d07741466c02cb98b18a16ee7b0
villetoimela <ville.toimela@hiisi.digital>
2022-10-04 10:13:42 +0300

    added text-center to PrimaryHeader component

--- components/primary-header.jsx
+++ components/primary-header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrimaryHeader = (props) => {
     return (
-        <div className="mb-20 space-y-5 sm:mx-auto sm:max-w-xl md:max-w-2xl sm:space-y-4 lg:max-w-5xl">
+        <div className="mb-20 space-y-5 sm:mx-auto text-center sm:max-w-xl md:max-w-2xl sm:space-y-4 lg:max-w-5xl">
             <h2 className="relative text-gray-900 font-AmaticSC text-6xl font-extrabold tracking-tight sm:text-8xl">{props.title}</h2>
             <p className="sm:text-xl text-gray-700">{props.text}</p>
         </div>
